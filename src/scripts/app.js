be82055a2cfa58d28db4687873cee8bb7b1228fe
Abdessine <111520588+Abdessine@users.noreply.github.com>
2025-03-12 12:24:14 +0100

    Presque fini
    Il manque panneau et changement de texte et fond pour desktop

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,8 @@
-console.info('Hello world');
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger) 
+
+gsap.to('#Vroom',{
+	scrollTrigger: '#Vroom',
+	
+	})
